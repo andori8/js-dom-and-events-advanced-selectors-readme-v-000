f6c2cb12748b1c9160a9b9a31d8ec6ef38028fab
Ryan Do <falcsmike7@gmail.com>
2017-11-20 11:20:52 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,19 @@
 function paragraphSelector() {
   return $('p');
 }
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('div.pics img#baby-ninja');
+}
+
+function divSelector() {
+  return $('div.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child');
+}
